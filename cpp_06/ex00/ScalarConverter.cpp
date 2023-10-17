6f6eefbf68a493fbfc4541be96799837cd420270
abouabra <aymanbouabra@gmail.com>
2023-10-17 18:53:22 +0100

    from pc

--- cpp_06/ex00/ScalarConverter.cpp
+++ cpp_06/ex00/ScalarConverter.cpp
@@ -1,5 +1,6 @@
 #include "ScalarConverter.hpp"
 #include <algorithm>
+#include <cctype>
 #include <string>
 
 ScalarConverter::ScalarConverter()
@@ -35,7 +36,7 @@ int detect_type(const char *s)
 	if(type != -1)
 		return (type);
 
-	if(str.length() == 1 && !isdigit(str[0]))
+	if(str.length() == 1 && !std::isdigit(str[0]))
 		return CHAR;
 	if(str.find_first_not_of("-0123456789") == std::string::npos)
 		return INT;
@@ -54,23 +55,55 @@ int detect_type(const char *s)
 	}
 	return UNKNOWN;
 }
-void display_char(char *str)
+void display_char(char *str, int type)
 {
 	char c;
-	
+	std::string s(str);
+
 	c = static_cast<char>(std::atoi(str));
-	if(c < 32 || c > 126)
+	if(check_for_special_cases(s) != -1 || type == UNKNOWN)
+		std::cout << "char: impossible" << std::endl;
+	else if(s.length() == 1 && !std::isdigit(s[0]))
+		std::cout << "char: '" << s << "'" << std::endl;
+	else if(c < 32 || c > 126)
 		std::cout << "char: Non displayable" << std::endl;
 	else
-		std::cout << "char: " << c << std::endl;
+		std::cout << "char: '" << c  << "'"<< std::endl;
+}
+
+void display_int(char *str, int type)
+{
+	int num;
+	std::string s(str);
+
+	num = static_cast<int>(std::atoi(str));
+	if(check_for_special_cases(s) != -1 || type == UNKNOWN)
+		std::cout << "int: impossible" << std::endl;
+	else if(type == CHAR)
+		std::cout << "int: " << static_cast<int>(str[0])  << std::endl;
+	else
+		std::cout << "int: " << num  << std::endl;
+}
+
+void display_float(char *str, int type)
+{
+	float num;
+	std::string s(str);
+
+	num = static_cast<float>(std::atof(str));
+	if(check_for_special_cases(s) != -1 || type == UNKNOWN)
+		std::cout << "float: impossible" << std::endl;
+	else
+		std::cout << "float: " << num << "f" << std::endl;
 }
 void ScalarConverter::convert(char *str)
 {
 	int type = detect_type(str);
 	std::cout << "type: " << type << std::endl << std::endl;
 
-	display_char(str);
-	std::cout << "int: " << std::atoi(str) << std::endl;
-	std::cout << "float: " << std::atof(str) << std::endl;
+	display_char(str, type);
+	display_int(str, type);
+	display_float(str, type);
+	// std::cout << "float: " << std::atof(str) << "f" << std::endl;
 	std::cout << "double: " << std::atof(str) << std::endl;
 }
