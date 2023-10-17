d0c366710544334f6eec1f7df933a74e71fd005b
Ayman Bouabra <abouabra@c2r4p1.1337.ma>
2023-10-17 23:55:15 +0100

    cpp_06: ex00: still working on

--- cpp_06/ex00/ScalarConverter.cpp
+++ cpp_06/ex00/ScalarConverter.cpp
@@ -59,7 +59,7 @@ void display_char(char *str, int type)
 {
 	char c;
 	std::string s(str);
-
+	
 	c = static_cast<char>(std::atoi(str));
 	if(check_for_special_cases(s) != -1 || type == UNKNOWN)
 		std::cout << "char: impossible" << std::endl;
@@ -85,6 +85,13 @@ void display_int(char *str, int type)
 		std::cout << "int: " << num  << std::endl;
 }
 
+std::string precision(float num)
+{
+	float test = num - static_cast<int>(num);
+	if(test == 0)
+		return (".0");
+	return ("");
+}
 void display_float(char *str, int type)
 {
 	float num;
@@ -93,8 +100,24 @@ void display_float(char *str, int type)
 	num = static_cast<float>(std::atof(str));
 	if(check_for_special_cases(s) != -1 || type == UNKNOWN)
 		std::cout << "float: impossible" << std::endl;
+	else if(type == CHAR)
+		std::cout << "float: " << static_cast<float>(str[0]) << precision(num) << "f" << std::endl;
 	else
-		std::cout << "float: " << num << "f" << std::endl;
+		std::cout << "float: " << num << precision(num) << "f" << std::endl;
+}
+
+void display_double(char *str, int type)
+{
+	double num;
+	std::string s(str);
+
+	num = static_cast<double>(std::atof(str));
+	if(check_for_special_cases(s) != -1 || type == UNKNOWN)
+		std::cout << "double: impossible" << std::endl;
+	else if(type == CHAR)
+		std::cout << "double: " << static_cast<double>(str[0]) << precision(num) << std::endl;
+	else
+		std::cout << "double: " << num << precision(num) << std::endl;
 }
 void ScalarConverter::convert(char *str)
 {
@@ -104,6 +127,5 @@ void ScalarConverter::convert(char *str)
 	display_char(str, type);
 	display_int(str, type);
 	display_float(str, type);
-	// std::cout << "float: " << std::atof(str) << "f" << std::endl;
-	std::cout << "double: " << std::atof(str) << std::endl;
+	display_double(str, type);
 }
