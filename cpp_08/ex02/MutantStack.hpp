403988bd4a4f91c08c34f9c6b9f39ac945c19e5f
Ayman Bouabra <abouabra@c2r4p1.1337.ma>
2023-10-22 23:51:48 +0100

    cpp_07: ex02: init files

---
+++ cpp_08/ex02/MutantStack.hpp
@@ -0,0 +1,20 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <algorithm>
+#include <iostream>
+#include <exception>
+#include <vector>
+#include <stack>
+
+class MutantStack : public std::stack<int>
+{
+	public:
+		// MutantStack();
+		// MutantStack(const MutantStack &copy);
+		// virtual ~MutantStack();
+		// MutantStack& operator=(const MutantStack &copy);
+
+};
+
+#endif
