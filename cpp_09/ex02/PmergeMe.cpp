1626e597aa920dca72369acda5a507bd61a0daf3
Ayman Bouabra <abouabra@c2r4p1.1337.ma>
2023-12-10 22:10:23 +0100

    added jacobsthal seq

--- cpp_09/ex02/PmergeMe.cpp
+++ cpp_09/ex02/PmergeMe.cpp
@@ -252,12 +252,45 @@ void PmergeMe::insertion_part(vec_vec_int &big_v, vec_int rest, int vec_size, ve
 	if(debug_mode)
 		print_main_and_pend_chain(main_chain, pend_chain, rest, vec_size);
 
-	for(vec_pair_iter it = pend_chain.begin(); it != pend_chain.end(); it++)
+    long long jacobsthal_diff[] = {
+        2u, 2u, 6u, 10u, 22u, 42u, 86u, 170u, 342u, 682u, 1366u,
+        2730u, 5462u, 10922u, 21846u, 43690u, 87382u, 174762u, 349526u, 699050u,
+        1398102u, 2796202u, 5592406u, 11184810u, 22369622u, 44739242u, 89478486u,
+        178956970u, 357913942u, 715827882u, 1431655766u, 2863311530u, 5726623062u,
+        11453246122u, 22906492246u, 45812984490u, 91625968982u, 183251937962u,
+        366503875926u, 733007751850u, 1466015503702u, 2932031007402u, 5864062014806u,
+        11728124029610u, 23456248059222u, 46912496118442u, 93824992236886u, 187649984473770u,
+        375299968947542u, 750599937895082u, 1501199875790165u, 3002399751580331u,
+        6004799503160661u, 12009599006321322u, 24019198012642644u, 48038396025285288u,
+        96076792050570576u, 192153584101141152u, 384307168202282304u, 768614336404564608u,
+        1537228672809129216u, 3074457345618258432u, 6148914691236516864u
+    };
+	
+	size_t i = 0;
+	while(pend_chain.size())
 	{
-		vec_vec_iter it2 = std::lower_bound(main_chain.begin(), it->second, it->first, comp_func);
-		main_chain.insert(it2, it->first);
-		update_pend_chain(pend_chain, it->second);
+		vec_pair_iter end = pend_chain.begin();
+		end += jacobsthal_diff[i++] -1;
+		while(true)
+		{
+			vec_vec_iter it2 = std::lower_bound(main_chain.begin(), end->second, end->first, comp_func);
+			main_chain.insert(it2, end->first);
+			update_pend_chain(pend_chain, end->second);
+			pend_chain.erase(end);
+
+			if(end == pend_chain.begin())
+				break;
+			end--;
+		}
+		// i++;
 	}
+
+	// for(vec_pair_iter it = pend_chain.begin(); it != pend_chain.end(); it++)
+	// {
+	// 	vec_vec_iter it2 = std::lower_bound(main_chain.begin(), it->second, it->first, comp_func);
+	// 	main_chain.insert(it2, it->first);
+	// 	update_pend_chain(pend_chain, it->second);
+	// }
 
 	make_v_from_main_and_pend_chain(main_chain, rest, v);
 	
