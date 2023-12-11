d9bc465236416dcc40533ae3f5d67feede9ced78
Ayman Bouabra <abouabra@c2r4p1.1337.ma>
2023-12-11 19:28:28 +0100

    done

--- cpp_09/ex02/main.cpp
+++ cpp_09/ex02/main.cpp
@@ -10,7 +10,12 @@ int main(int ac, char **av)
     av++;
 
     PmergeMe pm(av);
-    pm.set_debug_mode(true);
-    pm.merge_insertion_sort();
+    // pm.set_list_debug_mode(true);
+    // pm.set_vect_debug_mode(true);
+    pm.merge_insertion_sort_vector();
+    pm.merge_insertion_sort_list();
     return 0;
 }
+
+// AK-47 Test
+// while true; do clear && ./PmergeMe $(jot -r 21 1 10000 | tr '\n' ' ');sleep 0.1 ; done
